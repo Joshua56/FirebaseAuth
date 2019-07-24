f6d589fe3d8c252083c11d4c9eb814ffbdebbc29
Davidodari <davidkibzodari@gmail.com>
2019-07-24 15:17:59 +0300

    add fragment for disease symptoms

--- app/src/main/java/www/joshmyapps/com/healthcare/Medication.java
+++ app/src/main/java/www/joshmyapps/com/healthcare/Medication.java
@@ -1,12 +1,14 @@
 package www.joshmyapps.com.healthcare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Medication extends AppCompatActivity {
+
+    public static String disease;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +16,11 @@ public class Medication extends AppCompatActivity {
         setContentView(R.layout.activity_medication);
         Intent intent = getIntent();
 
-        if(intent.hasExtra("Disease")){
-        String disease = intent.getStringExtra("Disease");
-        TextView textview = findViewById(R.id.count_days_text_view);
-        textview.setText(disease);
+        if (intent.hasExtra("Disease")) {
+            disease = intent.getStringExtra(getString(R.string.disease_key));
+            Log.d(Medication.class.getSimpleName(), disease);
+            if (getSupportActionBar() != null)
+                getSupportActionBar().setTitle(disease);
         }
     }
 }
