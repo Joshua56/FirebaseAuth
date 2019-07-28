20df321e280f11f5e22681e2054a065b8d95f459
Davidodari <davidkibzodari@gmail.com>
2019-07-28 22:41:45 +0300

    add location details

--- app/src/main/java/www/joshmyapps/com/healthcare/DefaulterActivity.java
+++ app/src/main/java/www/joshmyapps/com/healthcare/DefaulterActivity.java
@@ -1,36 +1,100 @@
 package www.joshmyapps.com.healthcare;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import java.util.Locale;
+public class DefaulterActivity extends AppCompatActivity implements LocationListener {
 
-public class DefaulterActivity extends AppCompatActivity {
-
+    private static final int MY_PERMISSIONS_REQUEST_COARSE_LOCATION = 001;
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 002;
+    private String mLat;
+    private String mLon;
+    private TextView mTextView;
     private LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_defaulter);
-        mLocationManager = new LocationManager(this);
-        getLifecycle().addObserver(mLocationManager);
+        mTextView = findViewById(R.id.location_text_view);
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION) &&
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            }
+        } else {
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        }
     }
 
+    @SuppressLint("MissingPermission")
     @Override
-    protected void onResume() {
-        super.onResume();
-        final String lat = String.valueOf(mLocationManager.getLastLocation().get("latitude"));
-        final String lon = String.valueOf(mLocationManager.getLastLocation().get("longitude"));
-        Log.d(DefaulterActivity.class.getSimpleName(), lat);
-        TextView vTextView = findViewById(R.id.location_text_view);
-        vTextView.setText(String.format(Locale.getDefault(), "Longitude - %s \n\n Latitude - %s", lat, lon));
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_COARSE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+                } else {
+
+                }
+                return;
+            }
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+                } else {
+
+                }
+                return;
+            }
+
+        }
     }
 
     public void submitDefaulter(View view) {
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        String locationData = String.format("Longitude : %s \n\nLatitude : %s", location.getLongitude(), location.getLatitude());
+        mTextView.setText(locationData);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
 }
