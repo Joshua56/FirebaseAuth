6433463dbb0973075e7e624297fa06ba4f2a8323
Davidodari <davidkibzodari@gmail.com>
2019-07-28 18:52:17 +0300

    add defaulter layout

--- app/src/main/java/www/joshmyapps/com/healthcare/DefaulterActivity.java
+++ app/src/main/java/www/joshmyapps/com/healthcare/DefaulterActivity.java
@@ -3,6 +3,7 @@ package www.joshmyapps.com.healthcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class DefaulterActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class DefaulterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_defaulter);
+    }
+
+    public void submitDefaulter(View view) {
     }
 }
