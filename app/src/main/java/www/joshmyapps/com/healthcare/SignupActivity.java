ebe4cf47948009f8276349d5ffbcdcca49278e7f
Davidodari <davidkibzodari@gmail.com>
2019-07-29 20:13:43 +0300

    rename files

--- app/src/main/java/www/joshmyapps/com/healthcare/SignupActivity.java
+++ app/src/main/java/www/joshmyapps/com/healthcare/SignupActivity.java
@@ -27,7 +27,7 @@ public class SignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up__form);
+        setContentView(R.layout.activity_signup);
         if (getSupportActionBar() != null)
             getSupportActionBar().setTitle("Sign up");
 
