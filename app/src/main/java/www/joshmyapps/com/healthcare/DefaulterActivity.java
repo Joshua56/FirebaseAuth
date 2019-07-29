6b0f0ef7f156754b04732703ff279fcf9d88897b
Davidodari <davidkibzodari@gmail.com>
2019-07-29 22:47:11 +0300

    defaulter ui update

--- app/src/main/java/www/joshmyapps/com/healthcare/DefaulterActivity.java
+++ app/src/main/java/www/joshmyapps/com/healthcare/DefaulterActivity.java
@@ -1,12 +1,6 @@
 package www.joshmyapps.com.healthcare;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,9 +11,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -32,24 +23,22 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DefaulterActivity extends AppCompatActivity implements LocationListener {
+public class DefaulterActivity extends LocationActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_COARSE_LOCATION = 001;
-    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 002;
-    private String mLat = "";
-    private String mLon = "";
-    private TextView mLocationTextView;
-    private LocationManager mLocationManager;
+    private TextView mLatitudeTextView;
     private EditText mReasonEditText;
     private TextView mTrimesterTextView;
     private Spinner mDefaulterSpinner;
+    private TextView mLongitudeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_defaulter);
 
-        mLocationTextView = findViewById(R.id.location_text_view);
+        mLatitudeTextView = findViewById(R.id.latitude_text_view);
+        mLongitudeTextView = findViewById(R.id.longitude_text_view);
+
         mReasonEditText = findViewById(R.id.reason_edit_text);
         mTrimesterTextView = findViewById(R.id.days_text_view);
         mDefaulterSpinner = findViewById(R.id.defaulter_spinner);
@@ -74,48 +63,6 @@ public class DefaulterActivity extends AppCompatActivity implements LocationList
                 Toast.makeText(DefaulterActivity.this, "Invalid operation", Toast.LENGTH_LONG).show();
             }
         });
-
-        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION) &&
-                    ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
-            }
-        } else {
-            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_COARSE_LOCATION: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-                } else {
-
-                }
-                return;
-            }
-            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
-                } else {
-
-                }
-                return;
-            }
-
-        }
     }
 
     public void submitDefaulter(View view) {
@@ -147,8 +94,8 @@ public class DefaulterActivity extends AppCompatActivity implements LocationList
     Map<String, String> defaulterData() {
         return new HashMap<String, String>() {
             {
-                put("latitude", mLat);
-                put("longitude", mLon);
+                put("latitude", latitude);
+                put("longitude", longitude);
                 put("trimester", mTrimesterTextView.getText().toString());
                 put("reason", mReasonEditText.getText().toString());
             }
@@ -157,25 +104,10 @@ public class DefaulterActivity extends AppCompatActivity implements LocationList
 
     @Override
     public void onLocationChanged(Location location) {
-        String locationData = String.format("Longitude : %s \n\nLatitude : %s", location.getLongitude(), location.getLatitude());
-        mLocationTextView.setText(locationData);
-        mLat = String.valueOf(location.getLatitude());
-        mLon = String.valueOf(location.getLongitude());
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
+        latitude = String.valueOf(location.getLatitude());
+        longitude = String.valueOf(location.getLongitude());
+        mLatitudeTextView.setText(latitude);
+        mLongitudeTextView.setText(longitude);
     }
 
 }
